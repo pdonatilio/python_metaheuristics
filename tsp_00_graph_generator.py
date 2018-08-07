a08bfd871a02b910567319ea6fccd2d544a07de4
pdonatilio <pdonatilio@gmail.com>
2018-08-07 17:59:27 -0300

    Restructuring the Travel Salesman Examples

--- tsp_00_graph_generator.py
+++ tsp_00_graph_generator.py
@@ -4,12 +4,12 @@ import random
 import time
 
 
-class GraphGenerator(object):
+class GraphGenerator():
 
     def __init__(self, n):
         self.n = n
         self.graph = [[] for i in range(n)]
-        self.cost = {}
+        self.score = {}
 
     def generate_graph(self):
 
@@ -17,10 +17,10 @@ class GraphGenerator(object):
             # include an edge to all nodes
             for j in range(self.n):
                 if i != j:
-                    if ((i, j) and (j, i)) not in self.cost:
-                        cost = random.randint(1, 100)
-                        self.cost[(i, j)] = cost
-                        self.cost[(j, i)] = cost
+                    if ((i, j) and (j, i)) not in self.score:
+                        score = random.randint(1, 100)
+                        self.score[(i, j)] = score
+                        self.score[(j, i)] = score
 
                     self.graph[i].append(j)
 
@@ -28,5 +28,5 @@ class GraphGenerator(object):
         for i in range(self.n):
             print('Nodes connecteds to %d: ' % i, end=' ')
             for node in self.graph[i]:
-                print('[%d to %d the cost is %d]' % (i, node, self.cost[i, node]), end='\t ')
+                print('[%d to %d the score is %d] ' % (i, node, self.score[i, node]), end='\t ')
             print()
---
+++ tsp_02_simulated_annealing_random.py
@@ -0,0 +1,42 @@
+# Travel Salesman Problem in Python
+# Solved with Simulated Annealing (random method - Hamiltonian Cycle)
+
+from tsp_00_graph_generator import *
+
+
+def tsp_random(graph, loop):
+
+    best_circuit = []
+    best_score = None
+
+    def create_circuit(best_circuit, best_score):
+
+        edges = [i for i in range(1, graph.n)]
+        circuit = [0]
+        circuit_score = 0
+
+        while len(edges) > 0:
+            e = random.choice(edges)
+            edges.remove(e)
+            circuit_score += graph.score[(circuit[-1], e)]
+            circuit.append(e)
+
+        circuit_score += graph.score[(circuit[-1], 0)]
+
+        if best_score is None:
+            best_circuit = circuit[:]
+            best_score = circuit_score
+            print('Iter %d: Best circuit: %s - score %d' % (i + 1, str(best_circuit), best_score))
+        elif circuit_score < best_score:
+            best_circuit = circuit[:]
+            best_score = circuit_score
+
+        return(best_circuit, best_score)
+
+    for i in range(loop):
+        best_circuit, best_score = create_circuit(best_circuit, best_score)
+
+        # time.sleep(1)
+    print('Iter %d: Best circuit: %s - score %d' % (i + 1, str(best_circuit), best_score))
+
+
